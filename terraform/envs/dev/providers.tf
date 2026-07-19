0a91e46a7c72cd73d50a5ca2e2f3c7677934e8ae
Kelvin Onuchukwu <kelvin.onuchukwu@capgemini.com>
2026-07-19 09:10:08 +0200

    remove assume role

--- terraform/envs/dev/providers.tf
+++ terraform/envs/dev/providers.tf
@@ -1,10 +1,6 @@
 provider "aws" {
   region = var.aws_region
 
-  assume_role {
-    role_arn = "arn:aws:iam::${var.aws_account_id}:role/${var.terraform_execution_role_name}"
-  }
-
   default_tags {
     tags = var.tags
   }
